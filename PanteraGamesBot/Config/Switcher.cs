85e784a61bbb38c9ce77a95184bcf27b076987c6
Pantera5641 <Pantera5641@PANTERA5641-PC>
2024-08-27 18:51:58 +0300

    Добавьте файлы проекта.

---
+++ PanteraGamesBot/Config/Switcher.cs
@@ -0,0 +1,52 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace PanteraGamesBot.Config;
+
+internal static class Switcher
+{
+    private static bool _startSwitch;
+    private static bool _menuSwitch;
+
+    internal static void OnMessageRoot(ITelegramBotClient client, Update update)
+    {
+        switch (update.Message?.Text?.ToLower())
+        {
+            case "/start":
+                Start();
+                break;
+
+            case "/menu":
+                Menu();
+                break;
+        }
+    }
+    
+    private static void Start()
+    {
+        OffAll();
+        _startSwitch = true;
+    }
+
+    internal static bool GetStart()
+    {
+        return _startSwitch;
+    }
+    
+    private static void Menu()
+    {
+        OffAll();
+        _menuSwitch = true;
+    }
+    
+    internal static bool GetMenu()
+    {
+        return _menuSwitch;
+    }
+    
+    private static void OffAll()
+    {
+         _startSwitch = false;
+         _menuSwitch = false;
+    }
+}
---
+++ PanteraGamesBot/Main.cs
@@ -0,0 +1,22 @@
+﻿using PanteraGamesBot.CommandsList;
+using PanteraGamesBot.Config;
+
+namespace PanteraGamesBot;
+
+internal class Bot
+{
+    private static void Main()
+    {
+        Host panteragamesbot = new Host("7271288966:AAHJKYaAFCPyILZ_a27w8TqkbL-RXexASMA");
+        
+        Console.WriteLine("Бот запущен.");
+        
+        panteragamesbot.Start();
+        
+        panteragamesbot.OnMessage += Switcher.OnMessageRoot;
+        panteragamesbot.OnMessage += StartCommand.OnMessageStart;
+        panteragamesbot.OnMessage += MenuCommand.OnMessageMenu;
+        
+        Thread.Sleep(Timeout.Infinite);
+    }
+}
