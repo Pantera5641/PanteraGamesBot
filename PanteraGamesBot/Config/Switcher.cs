7a463fd10978fd9ece3216ea00e44d406c10096d
Pantera5641 <144065128+Pantera5641@users.noreply.github.com>
2024-08-28 20:36:41 +0300

    Adds PanteraGamesBot directory

---
+++ PanteraGamesBot/Config/Switcher.cs
@@ -0,0 +1,52 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace PanteraGamesBot.Config;
+
+internal static class Switcher
+{
+    private static bool _startSwitch;
+    private static bool _dungeonFighterSwitch;
+
+    internal static void OnMessageRoot(ITelegramBotClient client, Update update)
+    {
+        switch (update.Message?.Text?.ToLower())
+        {
+            case "/start":
+                Start();
+                break;
+
+            case "/dungeonfighter":
+                DungeonFighter();
+                break;
+        }
+    }
+
+    private static void Start()
+    {
+        OffAll();
+        _startSwitch = true;
+    }
+
+    internal static bool GetStart()
+    {
+        return _startSwitch;
+    }
+    
+    private static void DungeonFighter()
+    {
+        OffAll();
+        _dungeonFighterSwitch = true;
+    }
+    
+    internal static bool GetDungeonFighter()
+    {
+        return _dungeonFighterSwitch;
+    }
+    
+    internal static void OffAll()
+    {
+         _startSwitch = false;
+         _dungeonFighterSwitch = false;
+    }
+}
