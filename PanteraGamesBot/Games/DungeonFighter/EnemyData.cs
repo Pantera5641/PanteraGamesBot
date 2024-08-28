30ce9531e0e942cb09506d9afb587d8b0b9d6503
Pantera5641 <144065128+Pantera5641@users.noreply.github.com>
2024-08-28 20:05:51 +0300

    Again adds frontend for new game "DungeonFighter"

---
+++ PanteraGamesBot/Games/DungeonFighter/EnemyData.cs
@@ -0,0 +1,12 @@
+namespace PanteraGamesBot.Games.DungeonFighter;
+
+public class EnemyData
+{
+    internal static int _playerHP = 45;
+    internal static int _playerMP = 20;
+    
+    internal static int _playerAttack;
+    internal static int _playerTreatment;
+    internal static int _playerLostMP;
+    internal static int _playerAcquiredMP;
+}
